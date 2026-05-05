4a4e7e445a2604389c9acbbfb9c460c431c06a13
strikertutu <55476369+strikertutu@users.noreply.github.com>
2026-05-05 20:26:09 +0800

    Add files via upload

---
+++ main13.m
@@ -0,0 +1,89 @@
+%% clear
+clear;clc;
+rng("shuffle")
+%% parameters
+D=2;            % days to squeeze
+step = 60;
+stepnum = 1;
+Vs=590;          % number of nodes
+Lr=0;           % rate that a path would be adjust to "L" path, set it to 0
+L1r=10;         % rate that a path would be adjust to "L-1" path
+patience=10;    % times try to adjust
+b_min=10;       % b min
+b_max=50;      % original b max, which would be break if adjust
+c_min=1;        % c min
+c_max=10;      % c max
+Vt = Vs+step*(stepnum-1); 
+V = 100;
+E = V*V*0.1;
+%% LOOP
+iter=0;
+maxnum = 1000;
+res = zeros(1,maxnum);
+abnormal = zeros(0,0);
+while iter<maxnum
+    GenStart = tic;
+    G=create_dag12(V,b_min,b_max,c_min,c_max,Lr,L1r,E);
+    GenTime = toc(GenStart);
+    disp("Generate time:");
+    disp(GenTime);
+
+    tic;
+    [x, min_pay]=lp3(G,D);
+    LPTime = toc;
+    disp("LP time:");
+    disp(LPTime);
+
+    if(min_pay == 0)
+        disp("Not solvable");
+        continue
+    end
+
+    tic;
+    greed = 0;
+    %cuts_s = cell(D);
+    %cuts_t = cell(D);
+    %Gs=cell(D);
+    G1 = G;
+    for i=1:D
+        [mf,G1,cut_s,cut_t]=greedy3(G1);
+        %cuts_s{i} = cut_s;
+        %cuts_t{i} = cut_t;
+        %Gs{i} = G1;
+        greed = greed + mf;
+    end
+    
+    GreedyTime = toc;
+    disp("Greedy time:");
+    disp(GreedyTime)
+
+    res(iter+1) = greed/min_pay;
+
+
+    if min_pay<greed
+        disp('|LP| < |Greedy| !!!!!');
+        temp = greed/min_pay;
+        disp(temp);
+        abnormal = [abnormal,temp];
+        %save_graph(G,'fanli');
+        % fig1=print_graph(G,0,1);
+        % fig2=print_lp(G,x,2);
+        % fig3=print_greedy(Gs{1},cuts_s{1},cuts_t{1},3);
+        % fig4=print_greedy(Gs{2},cuts_s{2},cuts_t{2},4);
+        % waitfor(gcf);
+        %print_fig(fig1,fig2,fig3,fig4);
+        %break;
+    end
+    iter=iter+1;
+    disp(iter);  
+end
+if(size(abnormal))
+    writematrix(abnormal, 'abnormal.xlsx');
+    [F, X] = ecdf(abnormal); % 计算累积分布函数值和对应的X值
+     
+    figure;
+    plot(X, F, 'LineWidth', 2); % 绘制累积概率图
+    xlabel('Approximation Ratio');
+    ylabel('Cumulative Distribution');
+end
+%histogram(res);
